835256ecc812d6588382ec92c2c84edcd23f1ffe
Jumaru <spiralout.eu@gmail.com>
2023-10-22 22:12:01 +0300

    feat(API): :sparkles: enhance api/channels/[id] endpoint for extended data retrieval
    This commit extends the functionality of the api/channels/[id].tsx endpoint to provide a more comprehensive set of data regarding the channels and the announcements associated with them.
    Now, in addition to fetching the channel name and address from the AnnouncementHub contract, the endpoint also fetches all available info from the Announcement Channel contract, and paginates announcements and safe announcements by 10.
    
    Changes:
     - Imported necessary dependencies and ABI from the ./abi folder.
     - Added pagination functionality for fetching announcements and safe announcements.
     - Formatted the data for response to include channel name, channel address, paginated announcements and safe announcements.
    
     This enhancement allows for a more detailed view of channels and related announcements, aligning with the data structure of the Announcement Channel contract.

--- frontend/src/pages/api/channels/[id].ts
+++ frontend/src/pages/api/channels/[id].ts
@@ -1,21 +1,61 @@
 import { ethers } from 'ethers';
 import { NextApiRequest, NextApiResponse } from 'next';
-import ABI from '../../../abi/AnnouncementHub.json';
+import AnnouncementHubABI from '../../../abi/AnnouncementHub.json';
+import AnnouncementChannelABI from '../../../abi/AnnouncementChannel.json';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'GET') {
-    try {
-      const { id } = req.query;
-      const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
-      const contract = new ethers.Contract(process.env.HUB_CONTRACT_ADDRESS as string, ABI.abi, provider);
-      const channel = await contract.getChannel(Number(id));
-      res.status(200).json({ data: channel });
-    } catch (error: any) {
-      res.status(500).json({ error: error.message });
+    if (req.method === 'GET') {
+        const { id } = req.query;
+        const page: number = Number(req.query.page) || 1;
+        const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
+        const announcementHubContract = new ethers.Contract(process.env.HUB_CONTRACT_ADDRESS as string, AnnouncementHubABI.abi, provider);
+
+        try {
+            // Fetching channel name and address from AnnouncementHub contract
+            const [channelName, channelAddress] = await announcementHubContract.getChannel(Number(id));
+
+            // Connecting to AnnouncementChannel contract using the fetched address
+            const announcementChannelContract = new ethers.Contract(channelAddress, AnnouncementChannelABI.abi, provider);
+
+            // Fetching announcements and safe announcements
+            const totalAnnouncementsBigInt = await announcementChannelContract.announcementCount();
+            const totalSafeAnnouncementsBigInt = await announcementChannelContract.safeAnnouncementCount();
+            // Convert BigInt to number (be cautious if the BigInt value is very large)
+            const totalAnnouncements = Number(totalAnnouncementsBigInt.toString());
+            const totalSafeAnnouncements = Number(totalSafeAnnouncementsBigInt.toString());
+
+
+            // Calculate pagination parameters
+            const startIdx = (page - 1) * 10;
+            const endIdx = Math.min(startIdx + 10, totalAnnouncements);
+
+            // Fetching paginated announcements and safe announcements
+            const announcementsPromises = [];
+            const safeAnnouncementsPromises = [];
+            for (let i = startIdx; i < endIdx; i++) {
+                announcementsPromises.push(announcementChannelContract.getAnnouncement(i));
+                safeAnnouncementsPromises.push(announcementChannelContract.getSafeAnnouncement(i));
+            }
+            const announcements = await Promise.all(announcementsPromises);
+            const safeAnnouncements = await Promise.all(safeAnnouncementsPromises);
+
+            // Formatting the data for response
+            const responseData = {
+                channelName,
+                channelAddress,
+                announcements,
+                safeAnnouncements,
+                totalPages: Math.ceil(totalAnnouncements / 10),
+            };
+
+            res.status(200).json(responseData);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Server error' });
+        }
+    } else {
+        res.status(405).json({ error: 'Method not allowed' });
     }
-  } else {
-    res.status(405).json({ error: 'Method not allowed' });
-  }
 };
 
 export default handler;
